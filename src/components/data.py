0915de6450fb77bce8ac4a5a69abfb0070d4fcd0
GauravPatil8 <patilgauravpradeep@gmail.com>
2025-08-18 20:41:14 +0530

    Added mesh data handler

---
+++ src/components/data.py
@@ -0,0 +1,65 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
+import torch
+from src.utils.common import get_path
+from torch.utils.data import Dataset, DataLoader
+from src.utils.data import load_obj, normalize_mesh_to_bbox
+
+
+class MeshDataset(Dataset):
+    def __init__(self, dataset_dir: str, num_points: int = 2048):
+        """
+            Returns normalized and scaled to bounding box mesh
+            Parameters:
+                dataset_dir = location of stored data.
+                num_points = number of points to sample on the mesh
+        """
+        self.data_dir = dataset_dir
+        self.num_points = num_points
+        self.files = [get_path(root, file) for root, dirs, files in os.walk(dataset_dir) for file in files]
+        
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index: int):
+
+        mesh = load_obj(self.files[index])
+
+        # normalize and centralize the mesh
+        bounded_mesh = normalize_mesh_to_bbox(mesh, 1.0)
+
+        #sampling points on the surface of the bounded mesh
+        point_cloud = bounded_mesh.sample(self.num_points)
+
+        # return tuple(tensor of points (x, y, z), path of the original model)
+        return (torch.from_numpy(point_cloud), self.files[index])
+    
+
+if __name__ == '__main__':
+    
+    dataset = MeshDataset(R"C:\Padhai\implementation_research_papers\meshtron\artifacts\dataset", num_points=2048)
+
+    # Test __len__
+    print("Dataset size:", len(dataset))
+
+    # Test one item
+    points, path = dataset[32]
+    print("First mesh points shape:", points.shape)
+    print("File path:", path)
+
+    # Check range of points
+    print("Min:", points.min(axis=0))
+    print("Max:", points.max(axis=0))
+    print("Mean:", points.mean(axis=0))
+
+    # Wrap in dataloader
+    # loader = DataLoader(dataset, batch_size=4, shuffle=True)
+
+    # for batch in loader:
+    #     x, paths = batch
+    #     print("Batch points:", x.shape)
+    #     print("Paths:", paths)
+    #     break
