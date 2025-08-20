312947d440200e59bbf2ec024b8bdb76826fbb70
GauravPatil8 <patilgauravpradeep@gmail.com>
2025-08-20 20:51:11 +0530

    add root function

--- src/utils/common.py
+++ src/utils/common.py
@@ -3,3 +3,7 @@ import os
 def get_path(*args) -> str:
     "Joins and returns path (str)"
     return os.path.join(*args)
+
+def get_root_folder():
+    "returns the project's root folder path"
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
