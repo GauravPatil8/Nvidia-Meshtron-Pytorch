0a6c6f693ab196c9977b867cf24dead9029eb06a
GauravPatil8 <patilgauravpradeep@gmail.com>
2025-08-20 20:50:57 +0530

    add root function

--- src/config.py
+++ src/config.py
@@ -1,12 +1,12 @@
 import os
-from utils.common import get_path
+from utils.common import get_path, get_root_folder
 from config_entities import IngestionConfig
 
 class ConfigurationManager:
 
     @staticmethod
     def ingestion_config():
-        PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+        PROJECT_ROOT = get_root_folder()
         return IngestionConfig(
             root = get_path(PROJECT_ROOT, 'artifacts'),
             dataset = get_path(PROJECT_ROOT, 'artifacts', 'dataset'),
