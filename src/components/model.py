81037fa0ee7f3c378e6ba41817867f97d247158f
GauravPatil8 <patilgauravpradeep@gmail.com>
2025-08-24 15:50:37 +0530

    Init meshtron

---
+++ src/components/model.py
@@ -0,0 +1,64 @@
+import torch 
+import torch.nn as nn
+from src.components.HourglassTransformer import (
+    InputEmbedding,
+    build_hourglass_valley,
+    Transformer,
+    MultiHeadSelfAttention,
+    ProjectionLayer,
+    FeedForwardNetwork,
+    parse_hierarchy
+)
+class Meshtron(nn.Module):
+    def __init__(self,
+                 dim: int,
+                 vocab_size: int,
+                 n_heads: int,
+                 d_ff: int,
+                 hierarchy: str,
+                 dropout: float
+                 ):
+        super().__init__()
+        shortening_factor, num_blocks, n_pre_post_blocks = parse_hierarchy(hierarchy=hierarchy)
+        self.sf = shortening_factor
+        self.n_blocks = num_blocks
+        self.embedding = InputEmbedding(vocab_size, dim)
+        self.pre_blocks = nn.ModuleList([
+            Transformer(dim, 
+                        dropout, 
+                        MultiHeadSelfAttention(dim, n_heads, dropout, vocab_size), 
+                        FeedForwardNetwork(dim, d_ff, dropout)
+            ) for _ in range(n_pre_post_blocks)
+        ])
+        self.valley = build_hourglass_valley(
+            dim,
+            n_heads,
+            vocab_size,
+            self.sf,
+            self.n_blocks,
+            d_ff,
+            dropout
+        )
+                                             
+        self.post_block = nn.ModuleList([
+            Transformer(dim, 
+                        dropout, 
+                        MultiHeadSelfAttention(dim, n_heads, dropout, vocab_size), 
+                        FeedForwardNetwork(dim, d_ff, dropout)
+            ) for _ in range(n_pre_post_blocks)
+        ])
+        self.out_proj = ProjectionLayer(dim, vocab_size)
+
+    def forward(self, x, mask):
+        x = self.embedding(x)
+
+        for block in self.pre_blocks:
+            x = block(x, mask)
+
+        for layer in self.valley:
+            x = layer(x, mask)
+
+        for block in self.post_blocks:
+            x = block(x, mask)
+        
+        return self.out_proj(x)
