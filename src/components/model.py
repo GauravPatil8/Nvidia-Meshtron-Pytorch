2404ee11d68be6fcba4c62ac86004177475b6a6d
GauravPatil8 <patilgauravpradeep@gmail.com>
2025-08-29 01:54:49 +0530

    add cross attention

--- src/components/model.py
+++ src/components/model.py
@@ -11,6 +11,8 @@ from src.components.HourglassTransformer import (
     SwiGLU
 )
 from src.components.Attention import MultiHeadAttention
+from src.components.PerceiverEncoder import ConditioningEncoder
+
 
 class Meshtron(nn.Module):
     def __init__(self,
@@ -21,7 +23,15 @@ class Meshtron(nn.Module):
                  attn_window_size: int,
                  d_ff: int,
                  hierarchy: str,
-                 dropout: float
+                 dropout: float,
+                 use_conditioning: bool,
+                 con_num_latents:int,
+                 con_latent_dim:int,
+                 con_dim_ffn:int,
+                 con_num_blocks:int,
+                 con_n_attn_heads:int,
+                 con_num_self_attention_blocks: int,
+                 condition_every_n_layers: int,
                  ):
         super().__init__()
         shortening_factor, num_blocks, n_pre_post_blocks = parse_hierarchy(hierarchy=hierarchy)
@@ -29,13 +39,27 @@ class Meshtron(nn.Module):
         self.n_blocks = num_blocks
         self.embedding = InputEmbedding(vocab_size, dim)
         self.up_sample = LinearUpSample(shortening_factor)
+        self.use_conditioning = use_conditioning
+        self.point_cloud_conditioning = ConditioningEncoder(
+            num_latents=con_num_latents,
+            latent_dim=con_latent_dim,
+            dim_ffn=con_dim_ffn,
+            num_blocks=con_num_blocks,
+            heads=con_n_attn_heads,
+            num_self_attention=con_num_self_attention_blocks
+        )
+
         self.pre_blocks = nn.ModuleList([
-            Transformer(dim, 
-                        dropout, 
-                        MultiHeadAttention(dim, n_heads, dropout, rope_flag=True), 
-                        FeedForwardNetwork(dim, d_ff, dropout, SwiGLU)
-            ) for _ in range(n_pre_post_blocks)
+            Transformer(
+                dim,
+                dropout,
+                MultiHeadAttention(dim, n_heads, dropout, rope_flag=True),
+                FeedForwardNetwork(dim, d_ff, dropout, SwiGLU),
+                conditioning_flag= use_conditioning and (i % condition_every_n_layers == 0)
+            )
+            for i in range(n_pre_post_blocks)
         ])
+
         self.down_valley, self.center_layer, self.up_valley = build_hourglass_valley(
             dim,
             n_heads,
@@ -44,39 +68,57 @@ class Meshtron(nn.Module):
             self.sf,
             self.n_blocks,
             d_ff,
-            dropout
+            dropout,
+            condition_every_n_layers,
+            use_conditioning
         )
                                              
         self.post_block = nn.ModuleList([
             Transformer(dim, 
                         dropout, 
                         MultiHeadAttention(dim, n_heads, dropout, rope_flag=True), 
-                        FeedForwardNetwork(dim, d_ff, dropout, SwiGLU)
-            ) for _ in range(n_pre_post_blocks)
+                        FeedForwardNetwork(dim, d_ff, dropout, SwiGLU),
+                        conditioning_flag= use_conditioning and (i % condition_every_n_layers == 0)
+            ) for i in range(n_pre_post_blocks)
         ])
         self.out_proj = ProjectionLayer(dim, vocab_size)
 
-    def forward(self, x, mask):
+    def forward(self, x, conditioning_data, mask):
+
         skips = [] #holds skip connection values, used in upsampling
         x = self.embedding(x)
 
+        #conditioning tensor
+        cond= None
+
+        # Pre valley block
         for block in self.pre_blocks:
-            x = block(x, mask)
-        skips.append(x)
-        
+            cond = self.point_cloud_conditioning(conditioning_data) if block.use_conditioning else None
+            x = block(x=x, conditions=cond, mask=mask)
+        skips.append(x) # Appending residuals to be added later
+
+        #Downsampling valley
         for block in self.down_valley:
-            x = block(x, mask)
+            cond = self.point_cloud_conditioning(conditioning_data) if block.use_conditioning else None
+            x = block(x=x, conditions=cond, mask=mask)
             skips.append(x)
 
-        x = self.center_layer(x, mask)
+        #center layer of valley
+        x = self.center_layer(x = x, conditions = cond, mask = mask)
 
-        shift = self.sf - 1
+        
+        #upsampling valley
         for block, skip in zip(self.up_valley, reversed(skips)):
-            x = F.pad(x, (0,0,shift, -shift), value=0.) #causal padding for preventing leak
             x = self.up_sample(x, skip)
-            x = block(x, mask)
+            cond = self.point_cloud_conditioning(conditioning_data) if block.use_conditioning else None
+            x = block(x=x, conditions=cond, mask=mask)
 
+        #upsampling for the last vanilla block
+        x = self.up_sample(x, skips[0])
+
+        #last vanilla blocks
         for block in self.post_blocks:
-            x = block(x, mask)
+            cond = self.point_cloud_conditioning(conditioning_data) if block.use_conditioning else None
+            x = block(x=x, conditions=cond, mask=mask)
         
         return self.out_proj(x)
