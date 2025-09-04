ce20031efc986226b384a299be0131e20b20e9b8
GauravPatil8 <patilgauravpradeep@gmail.com>
2025-09-04 19:42:18 +0530

    update

--- src/components/VertexTokenizer.py
+++ src/components/VertexTokenizer.py
@@ -13,18 +13,12 @@ class VertexTokenizer:
         self.EOS = torch.tensor([bins+1], dtype=torch.int64)
         self.PAD = torch.tensor([bins+2], dtype=torch.int64) 
 
-        self.vocab_size = bins + 3
+        self.vocab_size = bins + 3 # add 3 for special tokens
 
-    def quantize(self, sequence: np.array):
+    def encode(self, sequence: np.array):
         "converts float values to discrete int bins"
         return torch.tensor(np.clip(np.floor((sequence + (self.box_dim / 2)) * (self.bins / self.box_dim)), 0, self.bins - 1)).to(dtype=torch.int64)
 
-    def dequantize(self, tokens: torch.Tensor):
+    def decode(self, tokens: torch.Tensor):
         "converts int bins to float values"
         return (tokens.float() / (self.bins - 1)) * self.box_dim - (self.box_dim / 2)
-    
-    def encode(self, sequence: torch.Tensor):
-
-        tokens = self.quantize(sequence)
-
-        return tokens
