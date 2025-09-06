09a915a70c3628bb3557aca1393bfad403332bb7
GauravPatil8 <patilgauravpradeep@gmail.com>
2025-09-06 14:58:09 +0530

    update ingestion

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
 from utils.common import get_path, get_root_folder
-from config_entities import IngestionConfig
+from config_entities import IngestionConfig, Params
 
 class ConfigurationManager:
 
@@ -9,6 +9,14 @@ class ConfigurationManager:
         PROJECT_ROOT = get_root_folder()
         return IngestionConfig(
             root = get_path(PROJECT_ROOT, 'artifacts'),
-            dataset = get_path(PROJECT_ROOT, 'artifacts', 'dataset'),
-            meshes = get_path(PROJECT_ROOT, 'mesh')
+            dataset_storage_dir = get_path(PROJECT_ROOT, 'artifacts', 'dataset'),
+            meshes = get_path(PROJECT_ROOT, 'mesh'),
+            dataset_len = 50
+        )
+    
+    @staticmethod
+    def model_params():
+        return Params(
+            dim = 12500,
+
         )
