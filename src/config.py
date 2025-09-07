98641a8bdd41da756140af648938e01628da7b0e
GauravPatil8 <patilgauravpradeep@gmail.com>
2025-09-07 15:05:13 +0530

    add setup configs

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import os
-from utils.common import get_path, get_root_folder
-from config_entities import IngestionConfig, Params
+from pathlib import Path
+from src.utils.common import get_path, get_root_folder
+from src.config_entities import IngestionConfig, ModelParams, TrainingConfig, DatasetConfig,DataLoaderConfig
 
 class ConfigurationManager:
 
@@ -15,8 +16,64 @@ class ConfigurationManager:
         )
     
     @staticmethod
-    def model_params():
-        return Params(
-            dim = 12500,
-
+    def trainig_config():
+        PROJECT_ROOT = get_root_folder()
+        return TrainingConfig(
+            learning_rate=0.01,
+            label_smoothing= 0.1,
+            model_folder=get_path(PROJECT_ROOT, "artifacts", "model"),
+            model_basename="meshtron",
+            preload="latest",
         )
+    
+    @staticmethod
+    def model_params():
+        return ModelParams(
+            dim = 12000,
+            embedding_size = 130, # 128 + 2,
+            n_heads = 64,
+            attn_window_size=3,
+            d_ff=512,
+            hierarchy="1@2 2@4 4@2 2@4 1@2",
+            dropout = 0.3,
+            use_conditioning = True,
+            con_num_latents= 1024,
+            con_latent_dim=512,
+            con_dim_ffn=512,
+            con_num_blocks=8,
+            con_n_attn_heads=64,
+            con_num_self_attention_blocks= 8,
+            condition_every_n_layers= 4         
+        )
+
+    @staticmethod
+    def dataset_config():
+        PROJECT_ROOT = get_root_folder()
+        return DatasetConfig(
+            dataset_dir=get_path(PROJECT_ROOT, 'artifacts', 'dataset'),
+            original_mesh_dir=get_path(PROJECT_ROOT, 'mesh'),
+            point_cloud_size=8192,
+            num_of_bins=128,
+            bounding_box_dim=1.0
+        )
+    
+    @staticmethod
+    def dataloader_config():
+        return DataLoaderConfig(
+            train_ratio=0.9,
+            batch_size=32,
+            num_workers=2,
+            shuffle=True,
+            pin_memory=False
+        )
+        
+def get_latest_weights_path(config: TrainingConfig):
+    weights_files = list(Path(config.model_folder).glob(config.model_basename))
+    if len(weights_files) == 0:
+        return None
+    weights_files.sort()
+    return str(weights_files[-1])
+
+def get_weights_path(config: TrainingConfig, epoch: str):
+    PROJECT_ROOT = get_root_folder()
+    return get_path(PROJECT_ROOT, config.model_folder, f"{config.model_basename}_{epoch}.pt")
--- src/config_entities.py
+++ src/config_entities.py
@@ -8,8 +8,45 @@ class IngestionConfig:
     dataset_len: int
 
 @dataclass
-class Params:
+class DatasetConfig:
+    dataset_dir: str
+    original_mesh_dir: str
+    point_cloud_size: int 
+    num_of_bins: int 
+    bounding_box_dim: float 
+    
+@dataclass
+class ModelParams:
     dim: int
-
+    embedding_size: int
+    n_heads: int
+    attn_window_size: int
+    d_ff: int
+    hierarchy: str
+    dropout: float
+    use_conditioning: bool
+    con_num_latents:int
+    con_latent_dim:int
+    con_dim_ffn:int
+    con_num_blocks:int
+    con_n_attn_heads:int
+    con_num_self_attention_blocks: int
+    condition_every_n_layers: int
+    
+@dataclass
 class TrainingConfig:
-    params : Params
+    num_epochs: int
+    model_folder: str
+    model_basename: str
+    learning_rate: float
+    label_smoothing: float
+    preload: str
+
+
+@dataclass
+class DataLoaderConfig:
+    train_ratio: float
+    batch_size: int
+    num_workers: int
+    shuffle: bool
+    pin_memory: bool
