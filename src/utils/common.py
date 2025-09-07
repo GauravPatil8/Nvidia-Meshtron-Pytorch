da5d01eb5d14b15ae6abb1e6b1ce21c196879e73
GauravPatil8 <patilgauravpradeep@gmail.com>
2025-09-07 15:05:33 +0530

    add logger

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 def get_path(*args) -> str:
     "Joins and returns path (str)"
@@ -7,3 +8,14 @@ def get_path(*args) -> str:
 def get_root_folder():
     "returns the project's root folder path"
     return os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+def logger_init():
+    os.makedirs(R"src/logs/", exist_ok=True)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+        handlers=[
+            logging.FileHandler(R"src/logs/info.log"),
+            logging.StreamHandler()
+        ]
+    )
