97bfb4652c191a73ebfe026228142dbfd714f144
GauravPatil8 <patilgauravpradeep@gmail.com>
2025-11-08 16:34:51 +0530

    Update: RotaryEmbeddings

--- meshtron/Attention.py
+++ meshtron/Attention.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from flash_attn import flash_attn_func
-from flash_attn.layers.rotary import apply_rotary_pos_emb, RotaryEmbedding
+from flash_attn.layers.rotary import RotaryEmbedding
 # from meshtron.rope import precompute_theta_pos_freq, apply_rope
 
 class Attention(nn.Module):
@@ -12,10 +12,11 @@ class Attention(nn.Module):
         assert dim % num_heads == 0, "dim must be divisible by num_heads"
         self.dim = dim
         self.num_heads = num_heads
-        self.head_dim = head_dim
+        self.head_dim = dim // num_heads
         self.window_size = window_size
         hidden_dim = num_heads * self.head_dim
 
+        self.rope = RotaryEmbedding(dim=self.head_dim, base=10000.0)
         self.q_proj = nn.Linear(dim, dim, bias=False,dtype=torch.float16)
         self.k_proj = nn.Linear(dim, dim, bias=False,dtype=torch.float16)
         self.v_proj = nn.Linear(dim, dim, bias=False,dtype=torch.float16)
@@ -33,17 +34,14 @@ class Attention(nn.Module):
         h = self.num_heads
         d = self.head_dim
         device = q.device
-        # q_freqs_complex = precompute_theta_pos_freq(self.head_dim, l_q, 10000.0)
-        # k_freqs_complex = precompute_theta_pos_freq(self.head_dim, l_k, 10000.0)
+
 
         q = self.q_proj(q).view(b_q, l_q, h, d)
         k = self.k_proj(k).view(b_k, l_k, h, d)
         v = self.v_proj(v).view(b_v, l_v, h, d)
-        rope = RotaryEmbedding(dim=self.dim, base=10000)
-        
+
         #positional embedding
-        q = apply_rotary_pos_emb(q, rope(seq_len = l_q))
-        k = apply_rotary_pos_emb(k, rope(seq_len = l_k))
+        q, k = self.rope(q, k)
 
         out = flash_attn_func(
             q, k, v,
