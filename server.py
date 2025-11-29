66e8c9c75775a28411b1cfcafe12ea51fdc3f4f3
GauravPatil8 <patilgauravpradeep@gmail.com>
2025-11-29 17:29:34 +0000

    added: token stream api

--- server.py
+++ server.py
@@ -4,8 +4,9 @@ import asyncio
 from pipeline.stages.inference import get_generator
 from pipeline.utils.common import get_path, get_root_folder
 import uvicorn
+import torch
 
-generator = get_generator(get_path(get_root_folder(), 'artifacts', 'meshtron_75.pt'))
+generator = get_generator(get_path(get_root_folder(), 'artifacts', 'models', 'meshtron_75.pt'))
 
 app = FastAPI()
 
@@ -16,14 +17,15 @@ async def generate_tokens(points, face_count, quad_ratio):
 
 @app.post("/stream")
 async def stream(data: dict):
-    points = data["point_cloud"]
-    face_count = data["face_count"]
-    quad_ratio = data["quad_ratio"]
+    points = torch.tensor(data["point_cloud"])
+    face_count = torch.tensor(data["face_count"])
+    quad_ratio = torch.tensor(data["quad_ratio"])
 
     return StreamingResponse(
         generate_tokens(points, face_count, quad_ratio),
-        media_type="text/event-stream"
+        media_type="text/event-stream",
+        headers={"Cache-Control": "no-cache", "Connection": "keep-alive"}
     )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
